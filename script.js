c80893e43159b7db178a264bf4e714b7db7078ea
NimraFatima <farimanim16@gmail.com>
2023-10-05 23:20:42 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// let HelloBtn = document.querySelector('button');
+// HelloBtn.addEventListener('click' , showMsg);
+
+// function showMsg(){
+//     alert('Asslam u Alaikm!');
+// }
+
+
+/*let HelloBtn = document.querySelector('button');
+HelloBtn.addEventListener('click' , inputMsg);
+
+function inputMsg(){
+   let name = prompt('Enter name of student');
+   HelloBtn.textcontent = 'Roll No. 1:' + name;
+}*/
